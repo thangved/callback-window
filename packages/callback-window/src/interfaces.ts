d783a4c64063e5063190510de3d9adb326702c08
Kim Minh Thắng <contact@thangved.com>
2025-09-13 00:14:51 +0700

    feat(callback-window): add startCallbackWindow function

--- packages/callback-window/src/interfaces.ts
+++ packages/callback-window/src/interfaces.ts
@@ -1,22 +1,24 @@
 export interface IStartCallbackWindowOptions {
   /**
-   * Url to open
-   */
-  url: string;
-  /**
    * Time to close window in milliseconds
+   * @default Infinity
    */
-  timeout?: number;
+  timeout: number;
+  /**
+   * Time to checking current url in milliseconds
+   * @default 100
+   */
+  interval: number;
   /**
    * The window checker should be closed. By default, the window will close when the origin of the window equals the origin of the parent window.
    * @param currentUrl - current url of the opened window
    */
-  shouldClose?: (currentUrl: string) => boolean | Promise<boolean>;
+  shouldClose: (currentUrl: string) => boolean | Promise<boolean>;
   /**
    * What to do when browser does not allow opening new windows, by default will change the current window address to the destination address
    * @param url
    */
-  onBrowserNotAllowed?: (url: string) => {};
+  onBrowserNotAllowed: (url: string) => void;
 }
 
 export interface IStartCallbackWindowResult {
@@ -24,5 +26,8 @@ export interface IStartCallbackWindowResult {
 }
 
 export interface IStartCallbackWindow {
-  (options: IStartCallbackWindowOptions): Promise<IStartCallbackWindowResult>;
+  (
+    url: string,
+    options: Partial<IStartCallbackWindowOptions>,
+  ): Promise<IStartCallbackWindowResult>;
 }
